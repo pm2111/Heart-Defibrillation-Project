d6189c44a8d8f2ee338d19741d7fffc58a9fcc35
petermarinov <peter_dimmar@yahoo.co.uk>
2015-12-09 11:58:30 +0000

    Fixed last_excited counter which was initialised to T (need it to be zero) and save excitability plot every 10^4 timesteps (solution was to also set last_excited to zero every time we save ) . Also, theres a python plot which visualises the plots. still need to make it save plots to a file

---
+++ python plotting 2/series_heatmaps.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+path = "/Users/petermarinov/msci project/plotting python 2/sequence of averaged frames light apd short sim/nu_0.2"
+
+
+filenames = []
+
+for f in os.listdir(path):
+    if not f.startswith('.'):
+        filenames.append(f)
+
+horizontal = 100
+
+total_time = 10000.
+# = os.listdir(path)
+runs = 4
+num_nu = np.size(filenames)/runs
+nu_min = 0.21
+nu_max = .34
+data = np.zeros((200,200))
+fraction = np.zeros((num_nu,runs))
+#data = np.genfromtxt("0.15_refractory_excited_light_restitution_200_healthy_pacemaker__100000.txt")
+cells = np.zeros((200,200))
+taus = np.zeros((200,200))
+for x in f:
+    for i in range (0,200):
+        for j in range (0,200):
+            cells[i,j] = 1 -data[j+200*i][0]/total_time
+            plt.figure()
+            plt.pcolor(cells)
+            plt.colorbar()
+            #plt.xlabel("nu")
+            #plt.ylabel("P risk")
+            plt.title( "Fracion of time spent in excited regime for nu = " )
+            plt.grid()
+plt.show()              
+    
+    
+
+
+nu = np.linspace(nu_min,nu_max,num_nu)
+plt.figure()
+plt.pcolor(cells)
+plt.colorbar()
+#plt.xlabel("nu")
+#plt.ylabel("P risk")
+plt.title( "Fracion of time spent in excited regime for nu = " )
+plt.grid()
+plt.show()
+plt.figure()
+plt.pcolor(-taus)
+plt.colorbar()
+#plt.xlabel("nu")
+#plt.ylabel("P risk")
+plt.title( "<tau> distribution after 10^5 time steps" )
+plt.grid()
+plt.show()
+
+
+
+
+#LOOK FOR:
+#average duration of episode (counter)
+#P risk: add column and divide by time (length of array)
